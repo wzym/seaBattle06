2e7e5e882fcf7af51e53cd586e73185465b7ae53
wzym <wzym@yandex.ru>
2015-03-24 10:43:36 +0300

    Ячейка в общих чертах завершена. Основа отображения состояния игры.

--- src/Model/Cell.java
+++ src/Model/Cell.java
@@ -1,5 +1,25 @@
 package Model;
 
-public enum Cell {
-    DECK, WATER, BUFFER, DAMAGED_DECK
+/**
+ * Из ячеек будет состоять игровое поле. Это карта-посредник между положением дел игрока, состоянием его флота
+ * и вьюшкой, а также отображением повреждённых кораблей и полей для противника.
+ */
+public class Cell {
+    private Status status;
+
+    public enum Status {
+        DECK, WATER, BUFFER, DAMAGED_DECK, DAMAGED_SHIP, DAMAGED_WATER
+    }
+
+    public Cell(Status status) {
+        this.setStatus(status);
+    }       // конструктор
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
 }
