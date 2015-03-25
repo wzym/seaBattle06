ccf86bfede7d1b25904645fc8e65bbd618c769ee
wzym <wzym@yandex.ru>
2015-03-25 12:26:23 +0300

    Запускаем сразу графический интерфейс.

--- src/FrontController.java
+++ src/FrontController.java
@@ -1,10 +1,12 @@
 import Model.Game;
+import View.View;
 import View.ViewTmp;
 
 public class FrontController {
     public static void main(String[] args) {
         Game game = new Game();
 
-        ViewTmp view = new ViewTmp(game.getPlayer1().getField());
+        //ViewTmp view = new ViewTmp(game.getPlayer1().getField());
+        View view1 = new View(game.getPlayer1().getField());
     }
 }
