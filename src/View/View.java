946cd3ff8af151ca43a52ae2c7cb5e6676b3d287
wzym <wzym@yandex.ru>
2015-03-25 18:26:24 +0300

    Добавлено меню.

--- src/View/View.java
+++ src/View/View.java
@@ -10,35 +10,50 @@ import Model.Player;
 
 public class View extends JFrame {
     private final JFrame frame = new JFrame("Морской бой 06");
-    private JButton[][] fieldOfFirstPlayer = new JButton[ConfigOfGame.get().height() + 1][ConfigOfGame.get().width() + 1];
-    private JButton[][] fieldOfSecondPlayer = new JButton[ConfigOfGame.get().height() + 1][ConfigOfGame.get().width() + 1];
+    private JMenuBar menuBar = new JMenuBar();
+    private JMenu game = new JMenu("Игра");
+    private JMenu about = new JMenu("Справка");
+    private JMenuItem restart = new JMenuItem("Запустить игру");
+    private JMenuItem exit = new JMenuItem("Выход");
+    private JButton[][] fieldOfFirstPlayer;
+    private JButton[][] fieldOfSecondPlayer;
     private char[] numberToLetter;
 
     {
+        game.add(restart);
+        game.add(exit);
+        menuBar.add(game);
+        menuBar.add(about);
+        fieldOfFirstPlayer = new JButton[ConfigOfGame.get().height() + 1][ConfigOfGame.get().width() + 1];
+        fieldOfSecondPlayer =  new JButton[ConfigOfGame.get().height() + 1][ConfigOfGame.get().width() + 1];
         this.numberToLetter = new char[]      // массив для вывода координаты "x" в буквенном кириллическом виде
                 {'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И', 'К', 'Л', 'М', 'Н', 'О', 'П',
                         'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Ы', 'Э', 'Ю', 'Я'};
     }
 
     public View(Cell[][] data1, Cell[][] data2) {
-
-        frame.setLayout(null);
+        frame.setLayout(new BorderLayout());
         frame.setSize(1100, 600);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         JPanel panel = new JPanel(new GridLayout(11, 11));
         JPanel panel2 = new JPanel(new GridLayout(11, 11));
-        panel.setLocation(50, 50);
-        panel2.setLocation(550, 50);
+        panel.setLocation(20, 20);
+        panel2.setLocation(520, 20);
         panel.setSize(480, 480);
         panel2.setSize(480, 480);
 
         this.initField(fieldOfFirstPlayer, panel, data1);
         this.initField(fieldOfSecondPlayer, panel2, data2);
 
-        frame.add(panel);
-        frame.add(panel2);
+        frame.add(menuBar, BorderLayout.NORTH);
 
+        Container container = getContentPane();
+        container.setLayout(null);
+        container.add(panel);
+        container.add(panel2);
+
+        frame.add(container, BorderLayout.CENTER);
         frame.setVisible(true);
     }
 
