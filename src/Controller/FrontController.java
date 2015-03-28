8ef77fa24653e1c8f7c10421006ec37b109c9fa2
wzym <wzym@yandex.ru>
2015-03-28 13:06:27 +0300

    Комментарий.

--- src/Controller/FrontController.java
+++ src/Controller/FrontController.java
@@ -1,5 +1,9 @@
 package Controller;
 
+/**
+ * Выполнены пункты 1-3, 5.
+ * Синглтон попробовал реализовать на ConfigOfGame, ArtificialIntelligence.ArtificialIntelligence
+ */
 import View.View;
 
 public class FrontController {
